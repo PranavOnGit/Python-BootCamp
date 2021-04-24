bcbe78db116993fa80b3f5e2c6f5c1ee6bc29a2e
Pranav Chavan <tasksontable@gmail.com>
2021-04-24 15:46:49 +0530

    Numbers is Python

---
+++ numbers.py
@@ -0,0 +1,31 @@
+
+from math import *  #To use Additional math functions which is called as MODULE
+
+my_num = 1;
+print(my_num);
+
+my_num = 10 % 2;  # - , * , /, %, +
+print(my_num);
+
+my_num = (2 * (3 + 5)) #we can do nuch complex calculations using parenthesis
+print(my_num);
+
+my_num = (2 * (3 + 5))
+print( str(my_num) + " this is my number");  #consversion to string 
+
+
+# Apart from basic functions we can use functions too 
+
+print (min(3, 7)) #to get min value ; 3
+print (max(3, 7)) #to get MAX value ; 7
+print (round(3.4)) #to get rounded value ; 3
+print (round(3.7)) #to get min value ; 5 
+print (abs(-7)) #to get absolute value ; 7
+print (pow(4, 2)) # to get 4^2
+
+
+# To use Additional functions we need to import math module in TOP
+
+print (floor(3.7)) #will get small number or round to lower num ; 3
+print (ceil(3.7)) #will get higher number or round to higher num ; 4
+print (sqrt(36)) #get squear root of the number ; 6.0
