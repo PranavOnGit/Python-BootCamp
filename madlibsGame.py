d9c81b83abbb66fb74319ed60eb49d38c0e7a344
Pranav Chavan <tasksontable@gmail.com>
2021-04-24 16:34:53 +0530

    Mad Libs game in Python

---
+++ madlibsGame.py
@@ -0,0 +1,17 @@
+
+
+#Mad Libs Game in Python
+
+animals= input("Enter a animal name : ")
+profession = input("Enter a profession name: ")
+cloth = input("Enter a piece of cloth name: ")
+things = input("Enter a thing name: ")
+name= input("Enter a name: ")
+place = input("Enter a place name: ")
+verb = input("Enter a verb in ing form: ")
+food = input("Food name: ")
+
+print("say " + food + ", the photographer said as the camera flashed! ") 
+print( name + " and I had gone to " + place +" to get our photos taken on my birthday. The first photo we really wanted was a picture of us dressed as ")
+print( animals + " pretending to be a " + profession + ". when we saw the second photo, it was exactly what I wanted. We both looked like ")
+print( things + " wearing " + cloth + " and " + verb + " exactly what I had in mind")
