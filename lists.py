f2e09bf79832294e86774403d697b96f4bc69b59
Pranav Chavan <tasksontable@gmail.com>
2021-04-24 19:22:58 +0530

    lists in Python

---
+++ lists.py
@@ -0,0 +1,9 @@
+
+
+fruites = ["Mango", "Chikoo", "Banana", "Sweet Lime", "Orange"]
+
+print(fruites)
+print(fruites[1])   #shows 2nd element from List
+print(fruites[1:])  #shows all elements from 2nd
+print(fruites[1:3]) #shows all elements btween 1 to 3
+print(fruites[-1])  #shows last element from list
